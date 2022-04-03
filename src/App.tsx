09167142751935952d8da86136011530709c8e8a
Kajal Singh <singh.kajal940@gmail.com>
2022-04-03 15:12:15 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import AppNavBar from './components/AppNavBar/AppNavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavBar />
+    </div>
+  );
+}
+
+export default App;
